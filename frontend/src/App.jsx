375a23c6b29d0f293c365d5e8bf553a194ceb1ee
Techie Mike <mngene4@gmail.com>
2024-05-04 16:57:24 +0100

    created Login Page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,17 @@
 import { useState } from 'react'
 import './App.css'
+import Login from './pages/login/Login';
+import Signup from './pages/signup/Signup'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className='p-4 h-screen flex items-center justify-center'>
+      {/* <Login /> */}
+      <Signup />
     </div>
   )
 }
 
-export default App
+export default App;
