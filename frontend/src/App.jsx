cebc50f8615b8abfa8f1c9050ac9eb2dfe16f3ae
Techie Mike <mngene4@gmail.com>
2024-05-05 01:09:42 +0100

    completed frontend design

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import Login from './pages/login/Login';
 import Signup from './pages/signup/Signup'
+import Home from './pages/home/Home';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,7 +10,8 @@ function App() {
   return (
     <div className='p-4 h-screen flex items-center justify-center'>
       {/* <Login /> */}
-      <Signup />
+      {/* <Signup /> */}
+      <Home />
     </div>
   )
 }
---
+++ frontend/src/components/sidebar/Conversations.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Conversation from './Conversation'
+
+export default function Conversations() {
+  return (
+    <div className='py-2 flex flex-col overflow-auto'>
+        <Conversation />
+    </div>
+  )
+}
