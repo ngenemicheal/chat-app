4280e50aaeff6f0652a482e148bae56b436a605e
Techie Mike <mngene4@gmail.com>
2024-05-06 23:15:40 +0100

    added send message functionality

--- frontend/src/components/messages/MessageContainer.jsx
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -1,16 +1,20 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Messages from './Messages'
 import MessageInput from './MessageInput'
 import {TiMessages} from 'react-icons/ti'
+import useCoversation from '../../zustand/useConversation'
 
 export default function MessageContainer() {
 
+  const {selectedConversation, setSelectedConversation} = useCoversation();
 
-  const noChatSelected = true;
+  useEffect(() => {
+    return () => setSelectedConversation(null);
+  }, [setSelectedConversation]);
 
   return (
     <div className='md:min-w-[450px] flex flex-col'>
-      {noChatSelected ? (
+      {!selectedConversation ? (
         <div className='flex items-center justify-center w-full h-full'>
           <div className="px-4 text-center sm:text-lg md:text-xl text-gray-200 font-semibold flex flex-col items-center gap-2">
             <p>Welcome 👋 John Doe ❄️</p>
@@ -22,7 +26,7 @@ export default function MessageContainer() {
         <>
           <div className='bg-slate-500 px-4 py-2 mb-2'>
               <span className='label-text'>To: </span>
-              <span className='text-gray-900 font-bold'>John Doe</span>
+              <span className='text-gray-900 font-bold'>{selectedConversation.fullName}</span>
           </div>
 
           <Messages />
